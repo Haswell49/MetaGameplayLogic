f88bc365641d376862d3cfbfc335dea86f1b2b94
Haswell49 <selimkayumov@gmail.com>
2024-09-26 00:43:17 +0500

    Added all models needed and added SQL DDL

--- src/server/db/models.py
+++ src/server/db/models.py
@@ -1,7 +1,21 @@
 from .base import Model
 
 
+class User(Model):
+    id: int
+    email: str
+    password: str
+
+
 class Item(Model):
-    id: int = 0
-    name: str = ""
-    price: int = 0
+    id: int
+    name: str
+    price: int
+    owner_id: int
+
+
+class Player(Model):
+    user_id: int
+    nickname: str
+    items: list[Item]
+    balance: int
---
+++ src/server/db/sql/create_tables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id       INTEGER PRIMARY KEY AUTOINCREMENT,
+    email    TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS players
+(
+    user_id  INTEGER PRIMARY KEY,
+    nickname TEXT    NOT NULL UNIQUE,
+    balance  INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS items
+(
+    id       INTEGER PRIMARY KEY AUTOINCREMENT,
+    name     TEXT    NOT NULL UNIQUE,
+    price    INTEGER NOT NULL,
+    owner_id INTEGER,
+    FOREIGN KEY (owner_id) REFERENCES players (user_id)
+)
