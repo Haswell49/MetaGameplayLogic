84064231ff3afd0871709f7b183eef934542468c
Haswell49 <selimkayumov@gmail.com>
2024-09-27 20:23:49 +0500

    Reworked entry point with proper db and aiohttp app setup

--- src/server/__main__.py
+++ src/server/__main__.py
@@ -1,9 +1,38 @@
-from aiohttp import web
+import asyncio
 
+import aiohttp_session
+from aiohttp import web
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
+
+import config
+import db.abstract
+import db.models
+from db.adapters import create_sql_adapter
+from db.mappers import create_mappers
 from urls import routes
 
-app = web.Application()
 
-app.add_routes(routes)
+def init_app(host: str, port: int, db_adapter: db.abstract.Adapter,
+             db_mappers: dict[db.abstract.Model, db.abstract.Mapper], loop: asyncio.AbstractEventLoop = None):
+    app = web.Application()
 
-web.run_app(app, host="127.0.0.1", port=8000)
+    app.db_adapter = db_adapter
+    app.db_mappers = db_mappers
+
+    aiohttp_session.setup(app, EncryptedCookieStorage(config.COOKIES_SECRET_KEY))
+
+    app.add_routes(routes)
+
+    web.run_app(app, host=host, port=port, loop=loop)
+
+
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+
+    sql_adapter = loop.run_until_complete(create_sql_adapter(config.SQL_FORMATTER_TYPE,
+                                                             config.SQL_ADAPTER_TYPE,
+                                                             config.DB_CONFIG))
+
+    sql_mappers = create_mappers(sql_adapter, config.SQL_MAPPER_TYPE, db.models)
+
+    init_app(config.SERVER_HOST, config.SERVER_PORT, sql_adapter, sql_mappers, loop=loop)
