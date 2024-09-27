04b4efdf15166ee0bd3e758e1329f1dbe2b71e1e
Haswell49 <selimkayumov@gmail.com>
2024-09-27 20:23:32 +0500

    Added host, port and db configuration variables to config.py

--- src/server/config.py
+++ src/server/config.py
@@ -1,4 +1,22 @@
+from db.adapters import SQLiteAsyncAdapter
+from db.formatters import SQLiteFormatter
+from db.mappers import SQLiteAsyncMapper
 
 SECRET_KEY = "ec5336a3f68b3518940bc211a7e5e45b5793f55632e20de92547d63ba4864014"
+
+COOKIES_SECRET_KEY = b"SGVsbG8sIFdvcmxkITIxNXQzMmdhYXMy"
+
 ENCRYPTION_ITER_COUNT = 20000
-q
+
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 8000
+
+DB_CONFIG = {
+    "database": "db.sqlite3"
+}
+
+SQL_ADAPTER_TYPE = SQLiteAsyncAdapter
+
+SQL_MAPPER_TYPE = SQLiteAsyncMapper
+
+SQL_FORMATTER_TYPE = SQLiteFormatter
