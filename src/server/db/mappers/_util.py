604a7180826fff7a6090e5cbf65af68e6d195e6f
Haswell49 <selimkayumov@gmail.com>
2024-09-27 20:20:51 +0500

    Fixed create_mappers factory

--- src/server/db/mappers/_util.py
+++ src/server/db/mappers/_util.py
@@ -1,4 +1,5 @@
 import types
+from inspect import isclass
 
 from .. import abstract
 from .. import base
@@ -6,11 +7,21 @@ from .. import base
 
 def create_mappers(adapter: abstract.Adapter,
                    mapper_type: type[abstract.Mapper],
-                   models: types.ModuleType):
+                   models: types.ModuleType) -> dict[type[abstract.Model], abstract.Mapper]:
+    result = dict()
+
     for model_name in vars(models):
         model_type = getattr(models, model_name)
+
+        if not isclass(model_type):
+            continue
 
         if not issubclass(model_type, base.Model):
             continue
 
-        yield mapper_type(adapter, model_type)
+        if model_type is base.Model:
+            continue
+
+        result[model_type] = mapper_type(adapter, model_type)
+
+    return result
