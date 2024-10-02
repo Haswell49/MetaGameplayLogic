3a01254b7b7e03fdf989efec37030c402b5c4236
Haswell49 <selimkayumov@gmail.com>
2024-10-02 12:26:52 +0500

    Minor fixes to config module

--- src/server/config.py
+++ src/server/config.py
@@ -9,14 +9,14 @@ from db.mappers import SQLiteAsyncMapper
 load_dotenv(".env.shared")
 load_dotenv(".env.sensitive")
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY").encode("utf-8")
 
-COOKIES_SECRET_KEY = os.environ.get("COOKIES_SECRET_KEY")
+COOKIES_SECRET_KEY = os.environ.get("COOKIES_SECRET_KEY").encode("utf-8")
 
-ENCRYPTION_ITER_COUNT = os.environ.get("ENCRYPTION_ITER_COUNT")
+ENCRYPTION_ITER_COUNT = int(os.environ.get("ENCRYPTION_ITER_COUNT"))
 
 SERVER_HOST = os.environ.get("SERVER_HOST")
-SERVER_PORT = os.environ.get("SERVER_PORT")
+SERVER_PORT = int(os.environ.get("SERVER_PORT"))
 
 DB_CONFIG = {
     "database": os.environ.get("DB_NAME")
